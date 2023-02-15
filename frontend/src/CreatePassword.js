2e2aa617f010d26297fec41d3d6fb43508f08d17
zijiahu <hhuzijia@gmail.com>
2023-02-15 11:20:05 -0800

    Add a form for user to create password
    This form asks the user to enter a password and also retype the password in order to confirm. The form will only be submitted if the password and also the confirmed password they entered are the same.
    I used web3.js to create an account for the user. After the account is created, the user will be redirected to another page, where their public address and private key will be displayed.

---
+++ frontend/src/CreatePassword.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+const Web3 = require('web3');
+
+function CreatePassword(){
+    const [password, setPassword] = useState();
+    const [passwordConfirm,setPasswordConfirm] = useState();
+    const [error,setError] = useState('');
+    const [disabled,setDisabled] = useState(true);
+
+    const navigate = useNavigate();
+
+    function createAddress(){
+        const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+        let account = web3.eth.accounts.create();
+        console.log("account: ", account);
+        let wallet = web3.eth.accounts.wallet.add(account);
+        console.log("wallet: ", wallet);
+        let keystore = wallet.encrypt(password);
+        console.log("keystore: ", keystore);
+
+        return {account: account, wallet: wallet, keystore: keystore};
+    }
+
+    function handleCreatePassword(evt){
+        evt.preventDefault();
+        console.log("submit clicked");
+
+        let result = createAddress();
+        console.log(result);
+
+        navigate('/display-address', {state: {address: result.account.address, privateKey: result.account.privateKey}});
+    }
+
+    useEffect(() => {
+        if(passwordConfirm !== '' && passwordConfirm !== password){
+            setError('Passwords Do Not Match');
+        }else{
+            setError('');
+        }
+    },[password, passwordConfirm]);
+
+    useEffect(() => {
+        if(password !== '' && passwordConfirm !== '' && password === passwordConfirm){
+            setDisabled(false);
+        }
+        else{
+            setDisabled(true);
+        }
+    },[password, passwordConfirm]);
+
+    return(
+        <React.Fragment>
+            <h1>Create Password</h1>
+            <div className="create-password">
+                <br />
+                <div className="form" onSubmit={handleCreatePassword}>
+                    <form className="create-password-form">
+                        <p>Enter Password{' '}</p>
+                        <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                        <p>Confirm Your Password{' '}</p>
+                        <p><input type="password" name="passwordconfirm" placeholder="Confirm Password" onChange={(e) => setPasswordConfirm(e.target.value)} /></p>
+                        <p style={{color: 'red'}}>{error}</p>
+                        <button type="submit" disabled={disabled} onClick={handleCreatePassword}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default CreatePassword;
