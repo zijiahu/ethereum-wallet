548d2b185d7c40fc84285ee62a9be1532704c8c0
zijiahu <hhuzijia@gmail.com>
2023-02-16 11:08:12 -0800

    Allow user to access wallet in display address page

--- frontend/src/DisplayAddress.js
+++ frontend/src/DisplayAddress.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { useLocation } from "react-router-dom";
+import React, {useState} from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 // import Web3 from "web3";
 const Web3 = require('web3');
 
 function DisplayAddress(){
     const location = useLocation();
-    console.log(location);
+    const navigate = useNavigate();
+
+    const [address, setAddress] = useState('');
+    const [privateKey, setPrivateKey] = useState('');
+
+    function handleClicked(evt){
+        evt.preventDefault();
+        navigate('/wallet');
+    }
+
+    if(location.state !== null && location.state.address !== null){
+        setAddress(location.state.address);
+    }
+
+    if(location.state !== null && location.state.privateKey !== null){
+        setPrivateKey(location.state.privateKey);
+    }
 
     return(
         <div>
             <h1>Create Address</h1>
             <h2>Public Address:</h2>
-            <div>{location.state.address}</div>
+            <div>{address}</div>
             <h2>Private Key:</h2>
-            <div>{location.state.privateKey}</div>
+            <div>{privateKey}</div>
+            <button onClick={handleClicked}>Go to My Wallet</button>
         </div>
     )
 }
