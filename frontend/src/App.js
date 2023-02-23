057a6d2b97a984822ceb271d55d7f84d2d891acb
zijiahu <hhuzijia@gmail.com>
2023-02-23 14:02:50 -0800

    Display NavBar on application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import DisplayAddress from './DisplayAddress';
 import WalletInfo from './WalletInfo';
 import SendTransaction from './SendTransaction';
 import Login from './Login';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar></NavBar>
           <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/create-new-wallet' element={<CreateNewWallet/>} />
