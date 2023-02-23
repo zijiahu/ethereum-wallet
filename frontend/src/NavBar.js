63d08a361a843f56f2dbed38322b10937331786b
zijiahu <hhuzijia@gmail.com>
2023-02-23 14:02:10 -0800

    Add NavBar to the application

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Navbar(){
+  return (
+      <div>
+        <ul>
+            <li><a href="/">Home</a></li>
+        </ul>
+      </div>
+  );
+}
+
+export default Navbar;
